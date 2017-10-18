e6660f5bd207ee7d6726fa9d1ec350af79c0b48c
ArthurBOliveira <arthurboliveira93@gmail.com>
2017-10-18 21:30:42 -0200

    Working on JWT

--- server/server.js
+++ server/server.js
@@ -3,11 +3,19 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const {
+  ObjectID
+} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
@@ -28,7 +36,9 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({
+      todos
+    });
   }, (e) => {
     res.status(400).send(e);
   });
@@ -46,7 +56,9 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({
+      todo
+    });
   }).catch((e) => {
     res.status(400).send();
   });
@@ -64,7 +76,9 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({
+      todo
+    });
   }).catch((e) => {
     res.status(400).send();
   });
@@ -85,12 +99,18 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({
+      todo
+    });
   }).catch((e) => {
     res.status(400).send();
   })
@@ -99,7 +119,9 @@ app.patch('/todos/:id', (req, res) => {
 // GET /users
 app.get('/users', (req, res) => {
   User.find().then((users) => {
-    res.send({users});
+    res.send({
+      users
+    });
   }, (e) => {
     res.status(400).send(e);
   });
@@ -110,8 +132,10 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then((user) => {
-    res.send(user);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
@@ -121,4 +145,6 @@ app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 
-module.exports = {app};
+module.exports = {
+  app
+};
