d85b6d4e2ccec805807f33c25e932898a0b70ea9
ArthurBOliveira <arthurboliveira93@gmail.com>
2017-10-20 00:36:56 -0200

    Hashing/Salting passwords!

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const jwt = require('jsonWebToken');
+const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -33,24 +34,64 @@ var UserSchema = new mongoose.Schema({
 });
 
 UserSchema.methods.toJSON = function () {
-  let user = this;
-  let userObject = user.toObject();
+  var user = this;
+  var userObject = user.toObject();
 
   return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
-  let user = this;
-  let access = 'auth';
-  let token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({
+    _id: user._id.toHexString(),
+    access
+  }, 'abc123').toString();
 
-  user.tokens.push({access, token});
+  user.tokens.push({
+    access,
+    token
+  });
 
   return user.save().then(() => {
     return token;
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User}
+module.exports = {
+  User
+}
--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ var {
 var {
   User
 } = require('./models/user');
+var {
+  authenticate
+} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -116,17 +119,6 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-// GET /users
-app.get('/users', (req, res) => {
-  User.find().then((users) => {
-    res.send({
-      users
-    });
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
 // POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
@@ -139,6 +131,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
